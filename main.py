71e7f8815a5f6afe94f32726ecefd52aee91d365
hritikp <hritik.pihal@oneture.com>
2024-11-16 17:32:53 +0530

    Resolve CORS err

--- main.py
+++ main.py
@@ -463,5 +463,7 @@ def get_user_blogs(user):
 
     except Exception as e:
         return jsonify({"error": f"Error fetching user blogs: {str(e)}"}), 500
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
